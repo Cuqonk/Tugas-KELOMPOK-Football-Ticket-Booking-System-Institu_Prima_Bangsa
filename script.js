d75f0dc7ae8d3446eea6313586ef1bc9a9eb998d
cuqonk-Deep <uqinsbadict06@gmail.com>
2024-12-24 10:53:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -120,15 +120,3 @@ function proceedToPayment(totalPrice) {
     alert(`Melanjutkan ke pembayaran melalui DANA untuk total Rp ${totalPrice.toLocaleString()}.`);
     window.location.href = "https://link.dana.id/m/checkout"; 
 }
-
-window.onload = function () {
-    isAdminLoggedIn = localStorage.getItem("isAdminLoggedIn") === "true";
-    updateSidebar();
-
-    if (isAdminLoggedIn) {
-        showSection("admin-panel");
-        showBookings();
-    } else {
-        showSection("home");
-    }
-};
